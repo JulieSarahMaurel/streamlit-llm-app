edd8209fe35aa4f1ee3790e4c120e28c770dece8
Julie Maurel <julie_fresh@hotmail.com>
2024-02-02 12:52:08 +0000

    new model and doc

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+from docx import Document
+import io
 import utils
 import vertex
 
@@ -30,24 +31,49 @@ with st.container():
                 ," \t \t Debug Model: ",st.session_state['debug_mode'])
 
 
-    prompt = st.text_area("Add your prompt: ",height = 100)
+    #prompt = st.text_area("Add your prompt: ",height = 100)
     
 ###trial julie
 
+    #uploaded_files = st.file_uploader("Choose a docx contract file", accept_multiple_files=True)
+    #for uploaded_file in uploaded_files:
+        #bytes_data = uploaded_file.read()
+        #st.write("filename:", uploaded_file.name)
+        #st.write(bytes_data)
+        
 
+
+    # Function to read the content of a docx file
+def read_docx(file):
+    doc = Document(file)
+    full_text = []
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+    return "\n".join(full_text)
+
+    # Create the user interface to upload the file   
+from docx import Document
+
+# Function to read the content of a docx file and save it to a variable
+def read_docx_and_save(file):
+    doc = Document(file)
+    full_text = []
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+    return "\n".join(full_text)
+
+st.title("Upload and Process DOCX File", type="docx")
+
+uploaded_file = st.file_uploader("Please upload your DOCX file here", type="docx")
+if uploaded_file is not None:
+    document_text = read_docx_and_save(uploaded_file)
+    document_text="Given the following contract text, extract uncapped contract liabilities: "+ document_text
 #####
-    
-    
-    
-
-    
-    if prompt:
-        st.session_state['prompt'].append(prompt)
+    if document_text :
+        #st.session_state['prompt'].append(prompt)
         st.markdown("<h3 style='text-align: center; color: blue;'>Generator Model Response</h3>", unsafe_allow_html=True)
         with st.spinner('PaLM is working to generate, wait.....'):
-            response = vertex.get_text_generation(prompt=prompt, temperature = st.session_state['temperature'],
-                                max_output_tokens = st.session_state['token_limit'],
-                                top_p = st.session_state['top_p'],
-                                top_k = st.session_state['top_k'])
+            response = vertex.get_text_generation(prompt=document_text)
+            #response = vertex.get_text_generation(prompt=prompt, temperature = st.session_state['temperature'],)
             st.session_state['response'].append(response)
             st.markdown(response)
